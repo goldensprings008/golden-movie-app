1aa993e3e507af6876349566f586cb5c3ad62f0a
Golden Springs <goldensprings008@gmail.com>
2026-09-08 14:05:15 +0300

    Fix GitHub Pages route navigation

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
       {/* Makes Favorites and Watchlist available across all pages. */}
       <MovieListsProvider>
         <App />
