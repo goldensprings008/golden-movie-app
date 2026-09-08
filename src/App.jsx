39b3e9fb9d60bac55e8baeea716b94cd9da3483b
Golden Springs <goldensprings008@gmail.com>
2026-09-08 14:02:09 +0300

    Reset scroll position on route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router";
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import HomePage from "./pages/HomePage";
@@ -8,10 +9,21 @@ import FavoritesPage from "./pages/FavoritesPage";
 import WatchlistPage from "./pages/WatchlistPage";
 import BottomNavigation from "./components/layout/BottomNavigation";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="flex min-h-screen flex-col bg-cinema-950 text-white">
       <Navbar />
+      <ScrollToTop />
 
       {/* Extra bottom space prevents mobile content from hiding behind the fixed bar. */}
       <main className="flex-1 pb-20 md:pb-0">
